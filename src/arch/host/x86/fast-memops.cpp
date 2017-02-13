0f38bdc454a5f508cb172978a55c043464d5f14c
Tom Spink <tspink@gmail.com>
2017-02-13 08:58:30 +0000

    Fix a serious flaw in the bzero routine, tweak the optised functions slightly and add more comments

--- src/arch/host/x86/fast-memops.cpp
+++ src/arch/host/x86/fast-memops.cpp
@@ -2,7 +2,7 @@
 
 extern "C" void __bzero(void *, size_t);
 extern "C" void __pzero(void *, size_t);
-extern "C" void __memcpy(void *, const void *, size_t);
+extern "C" void *__memcpy(void *, const void *, size_t);
 extern "C" void __memset(void *, int, size_t);
 
 using namespace vrt::util;
@@ -19,8 +19,7 @@ __strong void vrt::util::pzero(void* ptr, size_t count)
 
 __strong void *vrt::util::memcpy(void *dest, const void *src, size_t length)
 {
-	__memcpy(dest, src, length);
-	return dest;
+	return __memcpy(dest, src, length);
 }
 
 __strong void *vrt::util::memset(void *dest, int c, size_t length)
